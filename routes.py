55605dde6c18c40e5ccb9316419d97c01f2fe3e3
ktread <ktread@users.noreply.github.com>
2019-07-19 14:02:46 -0600

    adding more route data

--- routes.py
+++ routes.py
@@ -2,9 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get('https://14ers.com/routelist.php?peak=Grays+Peak+and+Torreys+Peak')
-html = BeautifulSoup(response.text, 'html.parser')
-routes = []
-for a in html.find_all('div', {'class': 'niceborder'}):
-#for a in html.find_all('a',href=True):
-	routes.append(a['href'])
-print(routes)
+soup = BeautifulSoup(response.text, 'html.parser')
+table_body = soup.find_all('tr')
+mtd = []
+## Puts all the meta route data into a very messy list
+[mtd.append(x.get_text(strip=True)) for x in table_body]
+[print(x) for x in mtd]
+
+
+
+
+
+
+
+
+
