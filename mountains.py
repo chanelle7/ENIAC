d4e146b9bc4045ef654978428f0fa94ee40b2a4c
Katherine Treadwell <ktread@KatherinesAir3.hsd1.co.comcast.net>
2019-07-21 13:40:04 -0600

    moving finished weather code

--- mountains.py
+++ mountains.py
@@ -1,86 +1,74 @@
 import re
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-import numpy
+import datetime
 
-#
-# response = requests.get('https://14ers.com/routes.php')
-# html = BeautifulSoup(response.text, 'html.parser')
-#
-#
-# def get_mountains():
-# 	mountain_data = []
-# 	td = html.find_all('tr')
-# 	mountain_data = ([row.get_text(strip=True) for row in td if row.get_text(strip=True)[:6] != 'Routes'])
-# 	return mountain_data
-#
-# mountain_data = get_mountains()
-#
-#
-# # Working on getting this into a proper data frame for later
-# def clean_mountain_data(mountain_data):
-# 	mountain_detail = []
-# 	reg = "(.+)(14,\d{3})(')(\d+|\*?)(.+)"
-# 	for i in range(0, len(mountain_data)):
-# 		match = re.match(reg,str(mountain_data[i]))
-# 		mountain = []
-# 		mountain.append(match.group(1))
-# 		mountain.append(int(match.group(2).replace(',','')))
-# 		mountain.append(int(match.group(4).replace('*','-1')))
-# 		mountain.append(match.group(5))
-# 		mountain_detail.append(mountain)
-# 	return mountain_detail
-#
-# mountains = clean_mountain_data(mountain_data)
-# df = pd.DataFrame(mountains,columns=['Peak','Elevation','Rank','Range']) #.set_index('Peak')
-#print(df['Peak'])
 
-# def get_route_list(html):
-# 	route_pages = set()
-# 	for divtag in html.find_all('div', {'class': 'niceborder'}):
-# 		for atag in divtag.find_all('a',href=True):
-# TODO: Do I need the .union here - didn't need it above
-# 			route_pages = route_pages.union({atag['href'] for tag in atag if atag['href'][0:6] == 'routel'})
-# 	return route_pages
-#
-# #route_pages = get_route_list()
-#
-# def get_routes(route_pages):
-# 	routes = set({})
-# 	for link in route_pages:
-# 		response = requests.get('https://14ers.com/' + link)
-# 		html = BeautifulSoup(response.text, 'html.parser')
-# 		table_body = html.find_all('tr')
-# 		[routes.add(x.get_text(strip=True)) for x in table_body if x.get_text(strip=True)[:4] != 'Trip']
-#
-# #routes = get_routes()
-#
-# def get_crowdsize():
-# 	crowd_size = []
-# 	response = requests.get('https://14ers.com/php14ers/peakusage.php')
-# 	html = BeautifulSoup(response.text, 'html.parser')
-# 	for divtag in html.find_all('table', {'class': 'MainText1'}):
-# 		[crowd_size.append(atag.get_text(strip=True)) for atag in divtag.find_all('tr')]
-#
-# #crowd_size = get_crowdsize()
-#
-# def get_status():
-# 	status = []
-# 	response = requests.get('https://14ers.com/php14ers/peakstatus_main.php')
-# 	html = BeautifulSoup(response.text, 'html.parser')
-# 	for div in html.find_all('table', {'class': 'peakTable'}):
-# 		for atag in div.find_all('a', href=True):
-# 			mt = re.search('14ers', atag['href'])
-# 			if mt:
-# 				status.append(atag.get_text(strip=True))
-# 				if len(atag.span['class']) == 1:
-# 					status.append('No Updates')
-# 				else:
-# 					status.append(atag.span['class'][1])
-# 	return status
+response = requests.get('https://14ers.com/routes.php')
+html = BeautifulSoup(response.text, 'html.parser')
+CONVERT = {1:0,2:12,3:24,4:36,5:48,6:60,7:72,8:84,9:96,10:108}
 
-# Pulling the raw HTML for the entire page
+def get_mountains():
+	td = html.find_all('tr')
+	mountain_data = ([row.get_text(strip=True) for row in td if row.get_text(strip=True)[:6] != 'Routes'])
+	return mountain_data
+
+def clean_mountain_data(mountain_data):
+	mountain_detail = []
+	reg = "(.+)(14,\d{3})(')(\d+|\*?)(.+)"
+	for i in range(0, len(mountain_data)):
+		match = re.match(reg,str(mountain_data[i]))
+		mountain = []
+		mountain.append(match.group(1))
+		mountain.append(int(match.group(2).replace(',','')))
+		mountain.append(int(match.group(4).replace('*','-1')))
+		mountain.append(match.group(5))
+		mountain_detail.append(mountain)
+	return mountain_detail
+
+def get_route_list(html):
+	route_pages = set()
+	for divtag in html.find_all('div', {'class': 'niceborder'}):
+		for atag in divtag.find_all('a',href=True):
+			if atag['href'][0:6] == 'routel':
+				route_pages.add(atag['href'])
+	return route_pages
+
+
+def get_routes(route_pages):
+	routes = set({})
+	for link in route_pages:
+		response = requests.get('https://14ers.com/' + link)
+		html = BeautifulSoup(response.text, 'html.parser')
+		table_body = html.find_all('tr')
+		[routes.add(x.get_text(strip=True)) for x in table_body if x.get_text(strip=True)[:4] != 'Trip' and x.get_text(strip=True)[0:12] != 'RoutesRoutes']
+	return list(routes)
+
+
+def get_crowdsize():
+	crowd_size = []
+	response = requests.get('https://14ers.com/php14ers/peakusage.php')
+	html = BeautifulSoup(response.text, 'html.parser')
+	for divtag in html.find_all('table', {'class': 'MainText1'}):
+		[crowd_size.append(atag.get_text(strip=True)) for atag in divtag.find_all('tr')]
+	return crowd_size
+
+
+def get_status():
+	status = []
+	response = requests.get('https://14ers.com/php14ers/peakstatus_main.php')
+	html = BeautifulSoup(response.text, 'html.parser')
+	for div in html.find_all('table', {'class': 'peakTable'}):
+		for atag in div.find_all('a', href=True):
+			mt = re.search('14ers', atag['href'])
+			if mt:
+				status.append(atag.get_text(strip=True))
+				if len(atag.span['class']) == 1:
+					status.append('No Updates')
+				else:
+					status.append(atag.span['class'][1])
+	return status
+
 def get_weather_urls():
 	response = requests.get('https://www.14ers.com/php14ers/weather.php')
 	html = BeautifulSoup(response.text, 'html.parser')
@@ -97,6 +85,7 @@ def get_weather_urls():
 			urls[mtn_name] = url
 	return urls
 
+
 def get_mountain_weather():
 	mtn_url = get_weather_urls()
 	reg = "(.+)(:)( \d+)(°F)(\d+-?\d+)(mph)(.+)"
@@ -111,40 +100,64 @@ def get_mountain_weather():
 		all_weather.append(weather)
 	return all_weather
 
-print(get_mountain_weather())
+
+def mountain_weather(all_weather):
+	weather_data = {}
+	for mtn in all_weather:
+		weather_data.update(clean_weather(mtn))
+	return weather_data
 
 
-def clean_weather():
-	raw = get_mountain_weather()
-	for i in raw:
-		print(i)
+def clean_weather(eachmtn):
+	ind = 0
+	dttmp = []
+	full_weather = {}
+	for item in eachmtn:
+		full_string = re.search("(.+:) (\d+)°F(\d+)-?(\d+)?(mph)(.+)",item)
+		day = re.search("(.+:)",item)
+		if day:
+			today = datetime.date.today()
+			j = datetime.datetime(today.year, today.month, today.day)
+			degree = full_string.group(2)
+			windslow = full_string.group(3)
+			windshigh = full_string.group(4) if full_string.group(4) else full_string.group(3)
+			desc = full_string.group(6)
+			wdate = (j + datetime.timedelta(hours=CONVERT[ind])).strftime('%m-%d-%Y %H:%M:%S')
+			dttmp.append((wdate,degree, windslow, windshigh, desc))
+		ind = ind+1
+	full_weather[eachmtn[0]] =tuple(dttmp)
+	return full_weather
 
 
+class Mountain():
+
+	def __init__(self, name, elevation):
+		self.name = name
+		self.elevation = elevation
+
+	def __repr__(self):
+		return self.name + ':' + self.elevation
 
 
-# #status  = get_status()
-#
-# class Mountain():
-#
-# 	def __init__(self, name, elevation):
-# 		self.name = name
-# 		self.elevation = elevation
-#
-# 	def __repr__(self):
-# 		return self.name + ':' + self.elevation
-#
-#
-#
-# # mountaindict = {x.name: x.elevation for x in mountains}
-#
-# # print(mountaindict)
-#
-#
-#
-#
-# # if __name__ == '__main__':
-# # 	get_mountains()
-# # 	get_route_list()
-# # 	get_routes()
-# # 	get_crowdsize()
-# # 	get_status()
+def call_data():
+	raw_mountain = get_mountains()
+	clean_mountain_data(raw_mountain)
+	route_pages = get_route_list(html)
+	routes = get_routes(route_pages)
+	crowdsize = get_crowdsize()
+	status = get_status()
+	all_weather = get_mountain_weather()
+	weather_data = mountain_weather(all_weather)
+	print('ROUTES: ',routes)
+	print('--'*50)
+	print('CROWD SIZE: ', crowdsize)
+	print('--'*50)
+	print('STATUS: ', status)
+	print('--'*50)
+	print('WEATHER DATA: ' ,weather_data)
+	print('--'*50)
+
+
+call_data()
+
+# if __name__ == '__main__':
--- weather.py
+++ weather.py
@@ -1,34 +1,62 @@
+
 import re
 import datetime
-convert = {1:0,2:12,3:24,4:36,5:48,6:60,7:72,8:84,9:96,10:108}
-data = ['Mt. Antero', 'TodayHigh: 60°F5-10mphChance Showers And Thunderstorms',
- 'TonightLow: 43°F10-15mphChance Showers And Thunderstorms then Mostly Cloudy',
- 'MondayHigh: 53°F5-10mphMostly Sunny then Chance Showers And Thunderstorms',
- 'MondayNightLow: 41°F5-15mphShowers And Thunderstorms Likely then Partly Cloudy',
- 'TuesdayHigh: 52°F10mphMostly Sunny then Chance Showers And Thunderstorms',
- 'TuesdayNightLow: 42°F5-10mphShowers And Thunderstorms Likely then Mostly Cloudy',
- 'WednesdayHigh: 55°F10-15mphPartly Sunny then Showers And Thunderstorms',
- 'WednesdayNightLow: 42°F10-15mphShowers And Thunderstorms Likely then Mostly Cloudy',
- 'ThursdayHigh: 53°F15mphPartly Sunny then Showers And Thunderstorms',
- 'ThursdayNightLow: 42°F10-15mphShowers And Thunderstorms then Mostly Cloudy',
- '', '']
 
-ind = 0
-for i in data:
-	full_string = re.search("(.+:)( \d+)(°F)(\d+-?\d+)(mph)(.+)",i)
-	day = re.search("(.+:)",i)
+wmtndata = ['Mt. Belford',
+           'TodayHigh: 59°F5-10mphChance Showers And Thunderstorms',
+           'TonightLow: 40°F10-20mphChance Showers And Thunderstorms then Partly Cloudy',
+           'MondayHigh: 48°F5-15mphMostly Sunny then Showers And Thunderstorms Likely',
+           'MondayNightLow: 38°F5-10mphShowers And Thunderstorms Likely then Partly Cloudy',
+           'TuesdayHigh: 47°F5-10mphMostly Sunny then Chance Showers And Thunderstorms',
+           'TuesdayNightLow: 39°F5-10mphShowers And Thunderstorms Likely then Mostly Cloudy',
+           'WednesdayHigh: 52°F10-15mphPartly Sunny then Showers And Thunderstorms',
+           'WednesdayNightLow: 40°F10-15mphShowers And Thunderstorms Likely then Mostly Cloudy',
+           'ThursdayHigh: 50°F15mphSlight Chance Showers And Thunderstorms then Showers And Thunderstorms',
+           'ThursdayNightLow: 39°F10-15mphShowers And Thunderstorms Likely then Mostly Cloudy',
+           '', ''], ['Mt. Bierstadt',
+                     'TodayHigh: 57°F6-10mphChance Showers And Thunderstorms',
+                     'TonightLow: 37°F7mphShowers And Thunderstorms Likely then Slight Chance Showers And Thunderstorms',
+                     'MondayHigh: 51°F6-16mphShowers And Thunderstorms Likely',
+                     'MondayNightLow: 36°F10-14mphChance Showers And Thunderstorms then Partly Cloudy',
+                     'TuesdayHigh: 55°F14-18mphMostly Sunny then Showers And Thunderstorms Likely',
+                     'TuesdayNightLow: 38°F9-14mphChance Showers And Thunderstorms then Partly Cloudy',
+                     'WednesdayHigh: 55°F16mphMostly Sunny then Showers And Thunderstorms Likely',
+                     'WednesdayNightLow: 39°F13mphChance Showers And Thunderstorms then Mostly Cloudy',
+                     'ThursdayHigh: 51°F14mphShowers And Thunderstorms',
+                     'ThursdayNightLow: 37°F9-14mphShowers And Thunderstorms Likely then Mostly Cloudy',
+                     '', '']
+
+
+def clean_weather(eachmtn):
+	ind = 0
 	dttmp = []
-	if day:
-		today = datetime.date.today()
-		degree = full_string.group(2)
-		j = datetime.datetime(today.year, today.month, today.day)
-		string = day.span()
-		day = day.group(1)
-		date = (j + datetime.timedelta(hours=convert[ind]))
-		dttmp.append([date, degree])
-	else:
-		pass
-	ind = ind+1
-	print(dttmp)
+	full_weather = {}
+	for item in eachmtn:
+		full_string = re.search("(.+:) (\d+)°F(\d+)-?(\d+)?(mph)(.+)",item)
+		day = re.search("(.+:)",item)
+		if day:
+			today = datetime.date.today()
+			j = datetime.datetime(today.year, today.month, today.day)
+			degree = full_string.group(2)
+			windslow = full_string.group(3)
+			windshigh = full_string.group(4) if full_string.group(4) else full_string.group(3)
+			desc = full_string.group(6)
+			wdate = (j + datetime.timedelta(hours=convert[ind])).strftime('%m-%d-%Y %H:%M:%S')
+			dttmp.append((wdate,degree, windslow, windshigh, desc))
+		ind = ind+1
+	full_weather[eachmtn[0]] =tuple(dttmp)
+	return full_weather
+
+
+def mountain_weather(allmtn):
+	weather_data = {}
+	for mtn in allmtn:
+		weather_data.update(clean_weather(mtn))
+	return weather_data
+
+
+final_weather = (mountain_weather(wmtndata))
+
+
 
 
